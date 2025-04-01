ecf8345e00be65aeb29d83771d37abfc35fb9b1b
serval19 <diwatomy04@gmail.com>
2025-04-01 21:59:00 +0530

    login

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,5 +1,6 @@
 const bcrypt=require('bcrypt');
 const UserModel=require('../Models/User')
+const jwt=require('jsonwebtoken');
 const signup=async (req,res)=>{
     try{
         const {name,email,password}=req.body;
@@ -18,6 +19,35 @@ const signup=async (req,res)=>{
         res.status(500).json({message:'internal server error',success:true});
     }
 }
+const login=async (req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const user=await UserModel.findOne({email});
+        const errorMessage='Auth failed email or passwrod is incorrect';
+        if(!user){
+            return res.status(403)
+            .json({message: errorMessage,success:false})
+        }
+        const isPassEqual=await bcrypt.compare(password,user.password);
+        if(!isPassEqual){
+            return res.status(403)
+            .json({message: errorMessage,success:false}) 
+        }
+        const jwToken=jwt.sign({email: user.email,_id: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: '24h'}
+        )
+        res.status(200).json({message:'login successfully',success:true,
+            jwToken,
+            email,
+            name:user.name 
+        });
+
+    }
+    catch(err){
+        res.status(500).json({message:'internal server error',success:true});
+    }
+}
 module.exports={
-    signup
+    signup,login
 }
--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,9 +1,10 @@
 const { signup } = require('../Controllers/AuthController');
 const { signupValidation }= require('../Middlewares/AuthValidation');
+const { login } = require('../Controllers/AuthController');
+const { loginValidation }= require('../Middlewares/AuthValidation');
 
 const router= require('express').Router();
-router.post('/login',(req,res)=>{
-    res.send('login successful');
-})
+
 router.post('/signup',signupValidation,signup);
+router.post('/login',loginValidation,login);
 module.exports=router;
