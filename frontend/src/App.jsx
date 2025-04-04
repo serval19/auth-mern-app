67974cb0c75fea7275a5c7acc8cb53e710a54de6
serval19 <diwatomy04@gmail.com>
2025-04-04 23:02:42 +0530

    signup page skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import  Login from './pages/Login'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import { Routes,Route, Navigate } from 'react-router-dom'
 const App = () => {
   return (
     <div className='App'>
-      <h1>Auth App</h1>
+      <Routes>
+        <Route path='/' element={<Navigate to='/login'/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/home' element={<Home/>} />
+      </Routes>
     </div>
   )
 }
 
-export default App
+export default App 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className='signup'>
+      <h1>Signup</h1>
+      <form>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' autoFocus placeholder='Enter your name'/>
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email'  placeholder='Enter your Email'/>
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input type='password' name='password'  placeholder='Enter your password'/>
+        </div>
+        <button>Signup</button>
+        <span>Already have an account?
+          <Link to='/login'>Login</Link>
+        </span>
+      </form>
+
+    </div>
   )
 }
 
