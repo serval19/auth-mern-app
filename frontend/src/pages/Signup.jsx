655dc2a2e755606cd9d1f56ca459b43636c25d13
serval19 <diwatomy04@gmail.com>
2025-04-08 21:43:34 +0530

    new

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ToastContainer} from 'react-toastify'
+import { handleSuccess,handleError } from '../utils'
 
 function Signup() {
   const [loginInfo,setloginInfo] = useState({
@@ -16,22 +17,31 @@ function Signup() {
     copyLoginInfo[name]=value
     setloginInfo(copyLoginInfo)
   }
-  console.log(loginInfo);
+  const handleSignup=(e)=>{
+    e.preventDefault();
+    const {name,email,password}=loginInfo;
+    if(!name || !email || !password){
+       return handleError('All fields are required!')
+    }
+  }
   return (
     <div className='signup'>
       <h1>Signup</h1>
-      <form>
+      <form onSubmit={handleSignup}>
         <div>
           <label htmlFor='name'>Name</label>
-          <input onChange={handleChange} type='text' name='name' autoFocus placeholder='Enter your name'/>
+          <input onChange={handleChange} type='text' name='name' autoFocus placeholder='Enter your name'
+          value={loginInfo.name}/>
         </div>
         <div>
           <label htmlFor='email'>Email</label>
-          <input onChange={handleChange} type='email' name='email'  placeholder='Enter your Email'/>
+          <input onChange={handleChange} type='email' name='email'  placeholder='Enter your Email'
+          value={loginInfo.email}/>
         </div>
         <div>
           <label htmlFor='password'>Password</label>
-          <input onChange={handleChange} type='password' name='password'  placeholder='Enter your password'/>
+          <input onChange={handleChange} type='password' name='password'  placeholder='Enter your password'
+          value={loginInfo.password}/>
         </div>
         <button>Signup</button>
         <span>Already have an account?
