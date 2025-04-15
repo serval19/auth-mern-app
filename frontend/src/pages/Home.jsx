e4bec8120337d6e4fc158e994c495e8cfade160a
serval19 <diwatomy04@gmail.com>
2025-04-15 17:02:29 +0530

    jwt token send along with the request

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { handleSuccess } from '../utils'
+import { handleError, handleSuccess } from '../utils'
 import { ToastContainer } from 'react-toastify'
 
-
 function Home() {
-  const [loggedInUser,setLoggedInuser]=useState("")
-  const navigate=useNavigate()
-  useEffect(()=>{
+  const [loggedInUser, setLoggedInuser] = useState("")
+  const navigate = useNavigate()
+  
+  useEffect(() => {
     setLoggedInuser(localStorage.getItem('loggedInUser'))
-  },[])
-  const handleLogout=()=>{
+  }, [])
+  
+  const handleLogout = () => {
     localStorage.removeItem('token')
     localStorage.removeItem('loggedInUser')
     handleSuccess('user logged out')
-    setTimeout(()=>{
+    setTimeout(() => {
       navigate('/login')
-    },1000)
+    }, 1000)
   }
+  
+  const fetchProducts = async () => {
+    try {
+      const url = "http://localhost:8080/products"
+      const headers={
+        headers:{
+          'Authorization': localStorage.getItem('token')
+        }
+      }
+      const response = await fetch(url,headers)
+      const result = await response.json()
+      console.log(result)
+    } catch (err) {
+      handleError(err)
+    }
+  }
+  
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+  
   return (
     <div>
       <h1>{loggedInUser}</h1>
